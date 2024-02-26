b81ec38afeadc4d2c9249ac45a186d1ae8659022
Anirban <anirbancodes@gmail.com>
2024-02-26 05:55:02 +0530

    added tailwind and alpine js for style

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,27 +1,29 @@
 @extends('app')
 
-@section('title', 'This is Index Page')
+@section('title', 'List of all tasks')
 
 @section('content')
 
+<nav class="mb-4">
+    <a href="{{ route('tasks.create') }}" class="link">Add Task!</a>
+</nav>
+
+@forelse($tasks as $task)
     <div>
-        <a href="{{route('tasks.create')}}">Add Task</a>
+        <a @class(['line-through'=> $task->completed])
+            href="{{ route('tasks.show', ['task' => $task->id]) }}">Title:
+            {{ $task->title }}</a>
     </div>
+@empty
+    <div>
+        <h2>No Data</h2>
+    </div>
+@endforelse
 
-    @forelse ($tasks as $task)
-        <div>
-            <a href="{{route('tasks.show', ['task' => $task->id])}}"><h2>Title: {{ $task->title }}</h2></a>
-        </div>
-    @empty
-        <div>
-            <h2>No Data</h2>
-        </div>
-    @endforelse
-
-    @if ($tasks->count())
-        <nav>
-            {{$tasks->links()}}
-        </nav>
-    @endif
+@if($tasks->count())
+    <nav class="mt-4">
+        {{ $tasks->links() }}
+    </nav>
+@endif
 
 @endsection
